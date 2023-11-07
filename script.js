a70f8fd1516b1d39ef75a2348fbb65aa38455be5
Susan Whiteford <swhiteford1218@gmail.com>
2023-11-07 14:19:05 -0500

    Rock Paper Scissors Game

---
+++ script.js
@@ -0,0 +1,51 @@
+Create array with Rock Paper Scissors inside array 
+
+Generate random numbers for both players between 0-2
+
+Two players: click on play: in js generate two random numbers one for player 1 and one for player 2
+
+If player 1 gets 0 then take array of 0 will return rock.  Array of 2 = Scissors
+
+
+
+let player1='rock' 
+//  let player2='rock'
+//  if(player1==='rock')
+//  {
+//     if(player2==='rock')
+//       console.log("Its a Tie")
+//     else if(player2==='paper')
+//     {
+//         console.log("Player1 wins")
+//     }
+//     else if(player2==='scissor')
+//     {
+//         console.log("Player1 wins")
+//     }
+//  }
+//  if(player1==='paper')
+//  {
+//     if(player2==='paper')
+//       console.log("Its a Tie")
+//     else if(player2==='rock')
+//     {
+//         console.log("Player2 wins")
+//     }
+//     else if(player2==='scissor')
+//     {
+//         console.log("Player2 wins")
+//     }
+//  }
+//  if(player1==='scissor')
+//  {
+//     if(player2==='scissor')
+//       console.log("Its a Tie")
+//     else if(player2==='rock')
+//     {
+//         console.log("Player2 wins")
+//     }
+//     else if(player2==='paper')
+//     {
+//         console.log("Player1 wins")
+//     }
+//  }
