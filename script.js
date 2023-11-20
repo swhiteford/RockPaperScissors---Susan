9e8e396bee5513e466ec822de2ceb906657618aa
Susan Whiteford <swhiteford1218@gmail.com>
2023-11-20 18:08:47 -0500

    updates

--- script.js
+++ script.js
@@ -1,80 +1,80 @@
-// // Create array with Rock Paper Scissors inside array
-// const playerText = document.querySelector("#playerText");
+const choices = ["rock", "paper", "scissors"];
+const computerChoiceElement = document.getElementById("computerImage");
+const resultTextElement = document.getElementById("resultText");
+const restartButton = document.querySelector(".restart");
 
-// const computerText = document.querySelector("#computerText");
+let playerScore = 0;
+let computerScore = 0;
 
-// const resultText = document.querySelector("#resultText");
+restartButton.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  computerChoiceElement.src = "placeholder.png";
+  resultTextElement.textContent = "";
+});
 
-// const choiceBtns = document.querySelectorAll(".choiceBtn");
+// function computerTurn() {
+//   const randNum = Math.floor(Math.random() * 3) + 1;
 
-let player;
-let computer;
-let playerPoints = 0;
-let computerPoints = 0;
-
-
-function computerTurn() {
-  const randNum = Math.floor(Math.random() * 3) + 1;
-
-  switch (randNum) {
-    case 1:
-      computer = "Rock";
-      return computer;
-      break;
-    case 2:
-      computer = "Paper";
-      return computer;
-    case 3:
-      computer = "Scissors";
-      return computer;
-      break;
-  }
-}
-
-
-function playerOption(playerChoice) {
-  //check if round <=10 then the remaining procedure should continue: or else alert "Game Over"
-  console.log("playerChoice:" + playerChoice);
-  computer = computerTurn();
-
-  comparechoiceGame (playerChoice.id, computer)
-  document.querySelector("#playerPointDisplay").textContent = playerPoints
-  document.querySelector("#computerPointDisplay").textContent = computerPoints
-}
+//   switch (randNum) {
+//     case 1:
+//       computer = "Rock";
+//       return computer;
+//       break;
+//     case 2:
+//       computer = "Paper";
+//       return computer;
+//     case 3:
+//       computer = "Scissors";
+//       return computer;
+//       break;
+//   }
+// }
 
 
-function comparechoiceGame (playerChoice, computer){
+// function playerOption(playerChoice) {
+//   //check if round <=10 then the remaining procedure should continue: or else alert "Game Over"
+//   console.log("playerChoice:" + playerChoice);
+//   computer = computerTurn();
 
-if (playerChoice === "Rock") {
-  if (computer === "Rock") alert("Its a Tie");
-  else if (computer === "Paper") {
-    alert("Computer wins");
-    computerPoints ++;
-  } else if (computer === "Scissors") {
-    alert("Player1 wins");
-    playerPoints++;
-  }
-}
-if (playerChoice === "Paper") {
-  if (computer === "Paper") alert("Its a Tie");
-  else if (computer === "Rock") {
-    alert("Player1 wins");
-    playerPoints++;
-  } else if (computer === "Scissors") {
-    alert("Computer wins")
-    computerPoints ++;
-  }
-}
-if (playerChoice === "Scissors") {
-  if (computer === "Scissors") alert("Its a Tie");
-  else if (computer === "Rock") {
-    alert("Computer wins");
-    computerPoints ++;
-  } else if (computer === "Paper") {
-    alert("Player1 wins");
-    playerPoints++;
-  }
-}
-}
+//   comparechoiceGame (playerChoice.id, computer)
+//   document.querySelector("#playerPointDisplay").textContent = playerPoints
+//   document.querySelector("#computerPointDisplay").textContent = computerPoints
+// }
+
+
+// function comparechoiceGame (playerChoice, computer){
+
+// if (playerChoice === "Rock") {
+//   if (computer === "Rock") alert("Its a Tie");
+//   else if (computer === "Paper") {
+//     alert("Computer wins");
+//     computerPoints ++;
+//   } else if (computer === "Scissors") {
+//     alert("Player1 wins");
+//     playerPoints++;
+//   }
+// }
+// if (playerChoice === "Paper") {
+//   if (computer === "Paper") alert("Its a Tie");
+//   else if (computer === "Rock") {
+//     alert("Player1 wins");
+//     playerPoints++;
+//   } else if (computer === "Scissors") {
+//     alert("Computer wins")
+//     computerPoints ++;
+//   }
+// }
+// if (playerChoice === "Scissors") {
+//   if (computer === "Scissors") alert("Its a Tie");
+//   else if (computer === "Rock") {
+//     alert("Computer wins");
+//     computerPoints ++;
+//   } else if (computer === "Paper") {
+//     alert("Player1 wins");
+//     playerPoints++;
+//   }
+// }
+// }
 
 
